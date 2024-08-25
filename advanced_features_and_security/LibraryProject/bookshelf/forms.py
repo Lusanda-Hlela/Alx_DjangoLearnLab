1791a49802738f72ef1b0d20d0ea3d05e2a0e692
Lusanda <hlelalusa@gmail.com>
2024-08-25 16:46:00 +0200

    Implementing Security Best Practices in Django_Version 1

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,11 +1,16 @@
 from django import forms
-from .models import Book
 
+class ExampleForm(forms.Form):
+  title = forms.CharField(max_length=100, required=True)
+  author = forms.CharField(max_length=100, required=True)
+  published_date = forms.DateField(widget=forms.SelectDateWidget)
 
-class BookForm(forms.ModelForm):
-  class Meta:
-    model = Book
-    fields = ["title", "author", "published_date"]
-    widgets = {
-      "published_date": forms.DateInput(attrs={"type": "date"}),
-    }
+  def clean_title(self):
+    data = self.cleaned_data["title"]
+    # Add custom validation for title here if necessary
+    return data
+
+  def clean_author(self):
+    data = self.cleaned_data["author"]
+    # Add custom validation for author here if necessary
+    return data
