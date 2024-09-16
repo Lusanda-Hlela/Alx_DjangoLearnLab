3dc5bfb3ab77a324e47964ca20978dcdb552d97a
Lusanda <hlelalusa@gmail.com>
2024-09-16 20:50:34 +0200

    Project Setup and User Authentication for a Social Media API Version 1

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,23 +1,24 @@
 from rest_framework import serializers
-from .models import CustomUser
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
+
+# Get the custom user model dynamically
+User = get_user_model()
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
   class Meta:
-    model = CustomUser
+    model = User
     fields = ['username', 'email', 'password', 'bio', 'profile_picture']
     extra_kwargs = {'password': {'write_only': True}}
 
   def create(self, validated_data):
-    user = CustomUser(
-      email=validated_data['email'],
+    user = User.objects.create_user(
       username=validated_data['username'],
+      email=validated_data['email'],
       bio=validated_data.get('bio', ''),
-      profile_picture=validated_data.get('profile_picture', None)
+      profile_picture=validated_data.get('profile_picture', None),
+      password=validated_data['password']  # Automatically hashes the password
     )
-    user.set_password(validated_data['password'])
-    user.save()
     Token.objects.create(user=user)
     return user
 
@@ -26,7 +27,7 @@ class UserLoginSerializer(serializers.Serializer):
   password = serializers.CharField(write_only=True)
 
   def validate(self, data):
-    user = CustomUser.objects.filter(username=data['username']).first()
+    user = User.objects.filter(username=data['username']).first()
     if user and user.check_password(data['password']):
       return user
     raise serializers.ValidationError("Incorrect Credentials")
