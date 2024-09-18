8ff48adbcae54819aaa587288b2c4103348b8eda
Lusanda <hlelalusa@gmail.com>
2024-09-18 21:00:50 +0200

    Implementing User Follows and Feed Functionality Version 0

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import status
+from rest_framework import status, generics
 from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authtoken.models import Token
 from .models import CustomUser
@@ -9,42 +8,51 @@ from .serializers import UserRegistrationSerializer, UserLoginSerializer, UserSe
 from posts.serializers import PostSerializer
 from posts.models import Post
 
-class UserRegistrationView(APIView):
+class UserRegistrationView(generics.GenericAPIView):
+  queryset = CustomUser.objects.all()
+  serializer_class = UserRegistrationSerializer
+
   def post(self, request):
-    serializer = UserRegistrationSerializer(data=request.data)
+    serializer = self.get_serializer(data=request.data)
     if serializer.is_valid():
       user = serializer.save()
       token, created = Token.objects.get_or_create(user=user)
       return Response({"token": token.key}, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class UserLoginView(APIView):
+class UserLoginView(generics.GenericAPIView):
+  queryset = CustomUser.objects.all()
+  serializer_class = UserLoginSerializer
+
   def post(self, request):
-    serializer = UserLoginSerializer(data=request.data)
+    serializer = self.get_serializer(data=request.data)
     if serializer.is_valid():
       user = serializer.validated_data
       token, created = Token.objects.get_or_create(user=user)
       return Response({"token": token.key}, status=status.HTTP_200_OK)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class FollowUserView(APIView):
+class FollowUserView(generics.GenericAPIView):
   permission_classes = [IsAuthenticated]
+  queryset = CustomUser.objects.all()
 
   def post(self, request, user_id):
     user_to_follow = get_object_or_404(CustomUser, id=user_id)
     request.user.following.add(user_to_follow)
     return Response({"detail": "User followed successfully"}, status=status.HTTP_200_OK)
 
-class UnfollowUserView(APIView):
+class UnfollowUserView(generics.GenericAPIView):
   permission_classes = [IsAuthenticated]
+  queryset = CustomUser.objects.all()
 
   def post(self, request, user_id):
     user_to_unfollow = get_object_or_404(CustomUser, id=user_id)
     request.user.following.remove(user_to_unfollow)
     return Response({"detail": "User unfollowed successfully"}, status=status.HTTP_200_OK)
 
-class FeedView(APIView):
+class FeedView(generics.GenericAPIView):
   permission_classes = [IsAuthenticated]
+  queryset = CustomUser.objects.all()
 
   def get(self, request):
     followed_users = request.user.following.all()
