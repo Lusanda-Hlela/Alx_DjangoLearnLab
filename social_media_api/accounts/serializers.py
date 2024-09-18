6ce1b6e02e13877020040626c370fac395369704
Lusanda <hlelalusa@gmail.com>
2024-09-18 20:55:08 +0200

    Implementing User Follows and Feed Functionality Version 0

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
+from posts.models import Post
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
   class Meta:
@@ -9,19 +10,10 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
     extra_kwargs = {'password': {'write_only': True}}
 
   def create(self, validated_data):
-    # Use get_user_model().objects.create_user() to create the user
-    user = get_user_model().objects.create_user(
-      username=validated_data['username'],
-      email=validated_data['email'],
-      password=validated_data['password']  # Automatically hashes the password
-    )
-    
-    # Set additional fields
+    user = get_user_model().objects.create_user(username=validated_data['username'], email=validated_data['email'], password=validated_data['password'])
     user.bio = validated_data.get('bio', '')
     user.profile_picture = validated_data.get('profile_picture', None)
     user.save()
-
-    # Create a token for the user
     Token.objects.create(user=user)
     return user
 
@@ -34,3 +26,8 @@ class UserLoginSerializer(serializers.Serializer):
     if user and user.check_password(data['password']):
       return user
     raise serializers.ValidationError("Incorrect Credentials")
+
+class UserSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = get_user_model()
+    fields = ['username', 'email', 'bio', 'profile_picture', 'followers', 'following']
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.authtoken.models import Token
 from .models import CustomUser
-from .serializers import UserRegistrationSerializer, UserLoginSerializer
-
+from .serializers import UserRegistrationSerializer, UserLoginSerializer, UserSerializer
+from posts.serializers import PostSerializer
+from posts.models import Post
 
 class UserRegistrationView(APIView):
   def post(self, request):
@@ -16,7 +18,6 @@ class UserRegistrationView(APIView):
       return Response({"token": token.key}, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
 class UserLoginView(APIView):
   def post(self, request):
     serializer = UserLoginSerializer(data=request.data)
@@ -25,3 +26,28 @@ class UserLoginView(APIView):
       token, created = Token.objects.get_or_create(user=user)
       return Response({"token": token.key}, status=status.HTTP_200_OK)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class FollowUserView(APIView):
+  permission_classes = [IsAuthenticated]
+
+  def post(self, request, user_id):
+    user_to_follow = get_object_or_404(CustomUser, id=user_id)
+    request.user.following.add(user_to_follow)
+    return Response({"detail": "User followed successfully"}, status=status.HTTP_200_OK)
+
+class UnfollowUserView(APIView):
+  permission_classes = [IsAuthenticated]
+
+  def post(self, request, user_id):
+    user_to_unfollow = get_object_or_404(CustomUser, id=user_id)
+    request.user.following.remove(user_to_unfollow)
+    return Response({"detail": "User unfollowed successfully"}, status=status.HTTP_200_OK)
+
+class FeedView(APIView):
+  permission_classes = [IsAuthenticated]
+
+  def get(self, request):
+    followed_users = request.user.following.all()
+    posts = Post.objects.filter(author__in=followed_users).order_by('-created_at')
+    serializer = PostSerializer(posts, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
